075680e44930221d101a96e122d2c29e01c9bf1c
OblivRuinDev <OblivRuinDev@qq.com>
2025-06-01 14:09:47 +0800

    Documents improve.

--- asm/src/main/java/org/objectweb/asm/Tag.java
+++ asm/src/main/java/org/objectweb/asm/Tag.java
@@ -28,57 +28,144 @@ package org.objectweb.asm;
 
 /**
  * Enumerate tags of each entry in Constant Pool.<br>
- * Or the possible values that may occur in Constant Pool.
+ * Or the possible values that may occur in Constant Pool.(like kinds for MethodHandle)
  * @see <a href="https://docs.oracle.com/javase/specs/jvms/se9/html/jvms-4.html#jvms-4.4">JVMS
  *     4.4</a></p>
  *
  * @author OblivRuinDev
  */
 public final class Tag {
-    /** The tag value of CONSTANT_Utf8 JVMS structures. */
+    /**
+     * The tag value of CONSTANT_Utf8 JVMS structures.
+     * @see <a href="https://docs.oracle.com/javase/specs/jvms/se24/html/jvms-4.html#jvms-4.4.7">
+     *     JVMS-4.4.7</a>
+     */
     public static final int Utf8 = 1;
-    /** The tag value of CONSTANT_Integer JVMS structures. */
+    /**
+     * The tag value of CONSTANT_Integer JVMS structures.
+     * @see <a href="https://docs.oracle.com/javase/specs/jvms/se24/html/jvms-4.html#jvms-4.4.4">
+     *     JVMS-4.4.4</a>
+     */
     public static final int Integer = 3;
-    /** The tag value of CONSTANT_Float JVMS structures. */
+    /**
+     * The tag value of CONSTANT_Float JVMS structures.
+     * @see <a href="https://docs.oracle.com/javase/specs/jvms/se24/html/jvms-4.html#jvms-4.4.4">
+     *     JVMS-4.4.4</a>
+     */
     public static final int Float = 4;
-    /** The tag value of CONSTANT_Long JVMS structures. */
+    /**
+     * The tag value of CONSTANT_Long JVMS structures
+     * @see <a href="https://docs.oracle.com/javase/specs/jvms/se24/html/jvms-4.html#jvms-4.4.5">
+     *     JVMS-4.4.5</a>
+     */
     public static final int Long = 5;
-    /** The tag value of CONSTANT_Double JVMS structures. */
+    /**
+     * The tag value of CONSTANT_Double JVMS structures.
+     * @see <a href="https://docs.oracle.com/javase/specs/jvms/se24/html/jvms-4.html#jvms-4.4.5">
+     *     JVMS-4.4.5</a>
+     */
     public static final int Double = 6;
-    /** The tag value of CONSTANT_Class JVMS structures. */
+    /**
+     * The tag value of CONSTANT_Class JVMS structures.
+     * @see Class
+     * @see <a href="https://docs.oracle.com/javase/specs/jvms/se24/html/jvms-4.html#jvms-4.4.1">
+     *     JVMS-4.4.1</a>
+     */
     public static final int Class = 7;
-    /** The tag value of CONSTANT_String JVMS structures. */
+    /**
+     * The tag value of CONSTANT_String JVMS structures.
+     * @see String
+     * @see <a href="https://docs.oracle.com/javase/specs/jvms/se24/html/jvms-4.html#jvms-4.4.3">
+     *     JVMS-4.4.3</a>
+     */
     public static final int String = 8;
-    /** The tag value of CONSTANT_Fieldref JVMS structures. */
+    /**
+     * The tag value of CONSTANT_Fieldref JVMS structures.
+     * @see <a href="https://docs.oracle.com/javase/specs/jvms/se24/html/jvms-4.html#jvms-4.4.2">
+     *     JVMS-4.4.2</a>
+     */
     public static final int Fieldref = 9;
-    /** The tag value of CONSTANT_Methodref JVMS structures. */
+    /**
+     * The tag value of CONSTANT_Methodref JVMS structures.
+     * @see <a href="https://docs.oracle.com/javase/specs/jvms/se24/html/jvms-4.html#jvms-4.4.2">
+     *     JVMS-4.4.2</a>
+     */
     public static final int Methodref = 10;
-    /** The tag value of CONSTANT_InterfaceMethodref JVMS structures. */
+    /**
+     * The tag value of CONSTANT_InterfaceMethodref JVMS structures.
+     * @see <a href="https://docs.oracle.com/javase/specs/jvms/se24/html/jvms-4.html#jvms-4.4.2">
+     *     JVMS-4.4.2</a>
+     */
     public static final int InterfaceMethodRef = 11;
-    /** The tag value of CONSTANT_NameAndType JVMS structures. */
+    /**
+     * The tag value of CONSTANT_NameAndType JVMS structures.
+     * @see <a href="https://docs.oracle.com/javase/specs/jvms/se24/html/jvms-4.html#jvms-4.4.6">
+     *     JVMS-4.4.6</a>
+     */
     public static final int NameAndType = 12;
-    /** The tag value of CONSTANT_MethodHandle JVMS structures. */
+    /**
+     * The tag value of CONSTANT_MethodHandle JVMS structures.
+     * @see java.lang.invoke.MethodHandle
+     * @see <a href="https://docs.oracle.com/javase/specs/jvms/se24/html/jvms-4.html#jvms-4.4.8">
+     *     JVMS-4.4.8</a>
+     * @since 1.7
+     */
     public static final int MethodHandle = 15;
-    /** The tag value of CONSTANT_MethodType JVMS structures. */
+    /**
+     * The tag value of CONSTANT_MethodType JVMS structures.
+     * @see java.lang.invoke.MethodType
+     * @see <a href="https://docs.oracle.com/javase/specs/jvms/se24/html/jvms-4.html#jvms-4.4.9">
+     *     JVMS-4.4.9</a>
+     * @since 1.7
+     */
     public static final int MethodType = 16;
-    /** The tag value of CONSTANT_Dynamic JVMS structures. */
+    /**
+     * The tag value of CONSTANT_Dynamic JVMS structures.
+     * @see <a href="https://docs.oracle.com/javase/specs/jvms/se24/html/jvms-4.html#jvms-4.4.10">
+     *     JVMS-4.4.10</a>
+     */
     public static final int Dynamic = 17;
-    /** The tag value of CONSTANT_InvokeDynamic JVMS structures. */
+    /**
+     * The tag value of CONSTANT_InvokeDynamic JVMS structures.
+     * @see <a href="https://docs.oracle.com/javase/specs/jvms/se24/html/jvms-4.html#jvms-4.4.10">
+     *     JVMS-4.4.10</a>
+     */
     public static final int InvokeDynamic = 18;
-    /** The tag value of CONSTANT_Module JVMS structures. */
+    /**
+     * The tag value of CONSTANT_Module JVMS structures.
+     * @see <a href="https://docs.oracle.com/javase/specs/jvms/se24/html/jvms-4.html#jvms-4.4.11">
+     *     JVMS-4.4.11</a>
+     * @since 9
+     */
     public static final int Module = 19;
-    /** The tag value of CONSTANT_Package JVMS structures. */
+    /**
+     * The tag value of CONSTANT_Package JVMS structures.
+     * @see <a href="https://docs.oracle.com/javase/specs/jvms/se24/html/jvms-4.html#jvms-4.4.12">
+     *     JVMS-4.4.12</a>
+     * @since 9
+     */
     public static final int Package = 20;
 
+    // =========== Kinds for MethodHandle ===========
+    /** Kind for {@link #MethodHandle}. */
     public static final int REF_getField         = 1;
+    /** Kind for {@link #MethodHandle}. */
     public static final int REF_getStatic        = 2;
+    /** Kind for {@link #MethodHandle}. */
     public static final int REF_putField         = 3;
+    /** Kind for {@link #MethodHandle}. */
     public static final int REF_putStatic        = 4;
+    /** Kind for {@link #MethodHandle}. */
     public static final int REF_invokeVirtual    = 5;
+    /** Kind for {@link #MethodHandle}. */
     public static final int REF_invokeStatic     = 6;
+    /** Kind for {@link #MethodHandle}. */
     public static final int REF_invokeSpecial    = 7;
+    /** Kind for {@link #MethodHandle}. */
     public static final int REF_newInvokeSpecial = 8;
+    /** Kind for {@link #MethodHandle}. */
     public static final int REF_invokeInterface  = 9;
+    /** Kind for {@link #MethodHandle}. */
 
     private Tag() {}
 }
